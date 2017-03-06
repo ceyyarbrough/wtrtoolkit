988f8447cbc785cad9538217eb4fec703bda97ca
ceyyarbrough <christopher.e.yarbrough@gmail.com>
2017-03-06 11:58:37 -0700

    fixed switch statement

--- wtrtoolkit/main.cpp
+++ wtrtoolkit/main.cpp
@@ -7,7 +7,12 @@ using namespace std;
 int uInputInt;
 char uInputChar;
 
+//installs scanallpro
+string scanAllPro(){
+    cout<<"Scan All Pro Installed"<<endl;
+}
 
+//selects printer driver
 string scannerSelection(){
     printf("Please select the Scanner Below\n");
     printf("Fujitsu:\n");
@@ -19,31 +24,46 @@ string scannerSelection(){
     cin >> uInputInt;
     switch(uInputInt){
     case 1:
-        return "Install 1";
+        cout<< "7160 installed" << endl;
         break;
 
     case 2:
-        return "Install 2";
+        cout<< "5120C" << endl;
         break;
 
     case 3:
-        return "Install 3";
+        cout<< "6120" << endl;
         break;
 
     case 4:
-        return "Install 4";
-
+        cout<< "6120z installed" << endl;
+        break;
+    default:
+        cout<< "error, try again" << endl;
+    }
+    printf("Have you installed Scan All Pro? y/n \n");
+    cin >> uInputChar;
+    if(uInputChar == 'y' || uInputChar == 'Y'){
+        printf("WTR is completed.... Exiting Program.");
+    } else if(uInputChar == 'n' || uInputChar == 'N'){
+        scanAllPro();
+    } else {printf("error, try again.");
     }
 }
 
-//test
-string scanAllPro(){
-    return "scanAllProInstall";
-}
+
 
 int main()
 {
-    printf("Has Scanner been installed?");
+    //user instructions
+    printf("WTR Tool Kit v1.0\n");
+    printf(" \n");
+    printf("This toolkit will install drivers used for WTR installation\n");
+    printf("Please follow Instructions Carefully....");
+    printf(" \n");
+    printf(" \n");
+
+    printf("Has Scanner been installed?\n");
     cin >> uInputChar;
 
     if(uInputChar == 'y' || uInputChar == 'Y'){
